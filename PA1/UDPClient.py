c128a3a539180bb286de5c70f1044cfabeb2d47c
Dan Sedano <dandano2006@gmail.com>
2020-05-03 22:53:32 -0700

    updated hostname

--- PA1/UDPClient.py
+++ PA1/UDPClient.py
@@ -1,10 +1,10 @@
 from socket import *
-serverName = 'hostname'
+serverName = gethostname() # gets the host's name
 serverPort = 12000
 
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = raw_input('Input lowercase sentence:')
-clientSocket.sendto(message.encode(),(servierName, serverPort))
-modifiedMessage, serverAddress = clientSocket.rcvfrom(2048)
+message = raw_input('Input lowercase sentence: ')
+clientSocket.sendto(message.encode(),(serverName, serverPort))
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 print(modifiedMessage.decode())
 clientSocket.close()
--- PA1/UDPServer.py
+++ PA1/UDPServer.py
@@ -1,6 +1,6 @@
 from socket import *
 
-serverPort = 1200
+serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 print("The server is ready to receive")
@@ -8,4 +8,3 @@ while True:
     message, clientAddress = serverSocket.recvfrom(2048)
     modifiedMessage = message.decode().upper()
     serverSocket.sendto(modifiedMessage.encode(), clientAddress)
-    
