6b2013ab1aa8f3a97806bbd67370a59bb6ab15f3
Dan Sedano <dandano2006@gmail.com>
2020-05-06 23:23:51 -0700

    changed code to match text book and added comments

--- PA1/TCPClient.py
+++ PA1/TCPClient.py
@@ -5,14 +5,32 @@
 # server client and receives a response and then decodes the response.
 
 
-import socket
-HOST = socket.gethostname()
-PORT = 1024
+# import socket
+# HOST = socket.gethostname()
+# PORT = 1024
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    message = input('Input lowercase sentence: ')
-    s.sendall(message.encode())
-    msg=s.recv(1024)
-    print(msg.decode("utf-8"))
-    s.close()
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.connect((HOST, PORT))
+#     message = input('Input lowercase sentence: ')
+#     s.sendall(message.encode())
+#     msg=s.recv(1024)
+#     print(msg.decode("utf-8"))
+#     s.close()
+
+from socket import *
+serverName = gethostname()
+serverPort = 12000
+# creates socket object with IPv4 Protocol (AF_INET) TCP Protocol (Sock_STREAM)
+clientSocket = socket(AF_INET, SOCK_STREAM)
+# establishes connection
+clientSocket.connect((serverName, serverPort))
+# asks for user input
+sentence = raw_input('Input lowercase sentence: ')
+# encodes the user input and transmits to server socket
+clientSocket.send(sentence.encode())
+# receives encoded returned response from server
+modifiedSentence = clientSocket.recv(1024)
+# decodes modifiedSentence and prints out to terminal
+print('From Server: ' + modifiedSentence.decode())
+# closes the socket
+clientSocket.close()
--- PA1/TCPServer.py
+++ PA1/TCPServer.py
@@ -5,20 +5,45 @@
 # the client and then converts the string to upper case letters and sends the 
 # messag back to the client
 
-import socket
-HOST = socket.gethostname() 
-PORT = 1024
+# import socket
+# HOST = socket.gethostname() 
+# PORT = 1024
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST,PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            modifiedMessage = data.decode().upper()
-            conn.sendall(modifiedMessage.encode())
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.bind((HOST,PORT))
+#     s.listen()
+#     conn, addr = s.accept()
+#     with conn:
+#         print('Connected', addr)
+#         while True:
+#             data = conn.recv(1024)
+#             if not data:
+#                 break
+#             modifiedMessage = data.decode().upper()
+#             conn.sendall(modifiedMessage.encode())
+
+from socket import *
+
+serverPort = 12000
+
+# creates socket object with IPv4 Protocol (AF_INET) TCP Protocol (Sock_STREAM)
+serverSocket = socket (AF_INET, SOCK_STREAM)
+# assigns a port number to the socket and does not assign an IP address.
+# Therefore, any client that sends a message to this port will be accepted.
+serverSocket.bind(('', serverPort))
+# makes the socket ready to accept a connections. 
+# The parameter (1) specifies the max queue of connections.
+serverSocket.listen(1)
+print('The server is ready to receive')
+while True:
+    # returns a connection socket object and the address of the bound socket from the client.
+    connectionSocket, add = serverSocket.accept()
+    # receives the message from the client and decodes the message
+    sentence = connectionSocket.recv(1024).decode()
+    # processes the message
+    capitalizedSentence = sentence.upper()
+    # encodes the message and then sends back to client
+    connectionSocket.send(capitalizedSentence.encode())
+    # closes the connection
+    connectionSocket.close()
